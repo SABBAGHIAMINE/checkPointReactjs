c42405379674f30e7ac072af58d7bebad3d23665
Amine Sabbagh <Amine.Sabbaghi@gmail.com>
2023-05-14 03:46:44 +0100

    check Point Formulaire

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import SimpleForm from "./components/SimpleForm";
+import MultiStepForm from './components/MultiStepForm';
+
+
+function App() {
+  return (
+    <div>
+      <MultiStepForm />
+    </div>
+  );   
+}
+
+export default App;
